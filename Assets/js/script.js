87c0f31dd81ceb9658267d55672991effdb1f2e7
toum21 <pey2moo1@gmail.com>
2023-09-21 18:20:18 -0700

    search bar allows user to search for the current weather and it displays in the console currently

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,20 +1,28 @@
 var weatherApiKey = '249506049aa732c8bd69f2c5fc992d9f';
-var waetherApiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&q='
+var weatherApiUrl = 'https://api.openweathermap.org/data/2.5/weather?q='
 
 var searchBtn = document.querySelector('#search-button');
-var searchBar = document.querySelector('#search-bar')
+var searchBar = document.querySelector('#search-bar');
 
-async function readWeather(city) {
+function currentWeather(city) {
+    var catURL = weatherApiUrl + city + `&appid=${weatherApiKey}&units=imperial`;
+    fetch(catURL)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
 
-};
 
 
 
 
 
-
-
-
-searchBtn.addEventListener('click', function () {
-    readWeather(searchBar.value);
+searchBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    city = searchBar.value;
+    currentWeather(city);
+    console.log(city)
 });
