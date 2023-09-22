3d61100cf8689c841212bc02d0697592931fa636
toum21 <pey2moo1@gmail.com>
2023-09-21 18:38:02 -0700

    updated search buttons text as well as updated the styling slightly on the application

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,5 @@
 var weatherApiKey = '249506049aa732c8bd69f2c5fc992d9f';
 var weatherApiUrl = 'https://api.openweathermap.org/data/2.5/weather?q='
-
 var searchBtn = document.querySelector('#search-button');
 var searchBar = document.querySelector('#search-bar');
 
